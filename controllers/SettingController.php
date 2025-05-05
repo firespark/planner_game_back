9fd509987e4d80eb15c5b6cea0fe82fcc1ad3584
fireleit <fireleit@gmail.com>
2025-05-05 17:40:41 +0400

    refactor: update settings method

--- controllers/SettingController.php
+++ controllers/SettingController.php
@@ -2,25 +2,24 @@
 require_once __DIR__ . '/../models/Setting.php';
 require_once __DIR__ . '/../core/Response.php';
 
-class SettingController {
+class SettingController
+{
     private $model;
 
-    public function __construct($db) {
+    public function __construct($db)
+    {
         $this->model = new Setting($db);
     }
 
-    public function get() {
-        $settings = $this->model->getAll();
-        Response::json($settings);
+    public function get()
+    {
+        Response::json($this->model->get());
     }
 
-    public function update($data) {
-        if (!isset($data['min_percentage'])) {
-            Response::json(['error' => 'Min Percentage is required'], 400);
-            return;
-        }
-
-        $this->model->update($data['min_percentage']);
+    public function update($data)
+    {
+        $this->model->update($data);
         Response::json(['success' => true]);
     }
 }
+
--- models/Setting.php
+++ models/Setting.php
@@ -1,23 +1,37 @@
 <?php
-class Setting {
+require_once __DIR__ . '/../config/Database.php';
+class Setting
+{
     private $conn;
     private $table = 'settings';
 
-    public function __construct($db) {
+    public function __construct($db)
+    {
         $this->conn = $db;
     }
 
-    public function getAll() {
-        $query = "SELECT * FROM {$this->table} LIMIT 1";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+    public function get()
+    {
+        $stmt = $this->conn->query("SELECT * FROM {$this->table} LIMIT 1");
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function update($minPercentage) {
-        $query = "UPDATE {$this->table} SET min_percentage = :min_percentage";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(':min_percentage', $minPercentage);
-        $stmt->execute();
+    public function update($data)
+    {
+        $stmt = $this->conn->prepare(
+            "UPDATE {$this->table} SET
+            project_start_date = :project_start_date,
+            segment_length = :segment_length,
+            total_segments = :total_segments,
+            minimum_percentage = :minimum_percentage"
+        );
+
+        return $stmt->execute([
+            ':project_start_date' => $data['project_start_date'],
+            ':segment_length' => $data['segment_length'],
+            ':total_segments' => $data['total_segments'],
+            ':minimum_percentage' => $data['minimum_percentage']
+        ]);
     }
 }
+
