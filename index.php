b91557c5015078d875a224a17608e86e2f97655b
fireleit <fireleit@gmail.com>
2025-05-13 19:03:25 +0400

    feat: start react project on server

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
-require_once __DIR__ . './routes/api.php';
 
-route();
+$uri = $_SERVER['REQUEST_URI'];
+
+if (strpos($uri, '/api/') === 0) {
+    require_once __DIR__ . '/routes/api.php';
+    route();
+    return;
+}
+
+if (preg_match('/\.(css|js|woff2?|eot|ttf|svg|png|jpg|jpeg|gif)$/', $uri)) {
+    $filePath = __DIR__ . '/dist' . $uri;
+
+    if (file_exists($filePath)) {
+
+        echo file_get_contents($filePath);
+        exit;
+    }
+}
+
+$indexFile = __DIR__ . '/dist/index.html';
+if (file_exists($indexFile)) {
+    echo file_get_contents($indexFile);
+} else {
+    http_response_code(404);
+    echo "404 Not Found";
+}
